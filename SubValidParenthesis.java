1dae39d8cb7a2e389735932b75208ce1263df100
surajkumar0207 <surajksingh0207@gmail.com>
2024-03-13 12:46:37 +0530

    Code Updated

--- SubValidParenthesis.java
+++ SubValidParenthesis.java
@@ -1,73 +1,92 @@
 package javaPrograms;
 
+//{[]()}({){}{][) => {[]()}{}
 import java.util.*;
 public class SubValidParenthesis
 {
-	//{[])()
-	static String sn="";
-	static int cou=0;
-	static List<Character> li=new ArrayList<>();
-	static List<String> lis=new ArrayList<>();
-	public static void reducedParenthesis(String s)
-	{
+	
+	public static void main(String[]args) {
+	    Scanner sc=new Scanner(System.in);
+	    String s=sc.next();
+	    System.out.println(parenthesis(s));
+	}
+	
+	static String parenthesis(String s) {
 	    int len=s.length();
-	   
-        sn=check(s,len);
-        if(sn.isEmpty())
-            return;
-        else
-            lis.add(sn);
-	    sn="";
-	    li.clear();
-	}
-	public static String check(String s,int len) {
-	    int rot=len;
-	    String f=""; String l="";
-	    for(int i=0;i<len/2;i++) {
-	        char c= s.charAt(i);  char ch=s.charAt(rot-i-1) ;
-	        if(c=='[' && (ch==']')) {
-	            f=f+c;
-	            l=ch+l;
+	    String ss=""; String s3="";
+	    Stack<Character> st=new Stack<>();
+	    for(int i=0;i<len;i++) {
+	        char ch=s.charAt(i);
+	        if(st.isEmpty() && (ch==']' || ch=='}' || ch==')'))
+	            continue;
+	        else if(ch=='{' || ch=='[' || ch=='(') {
+	            st.push(ch);
+	            ss+=ch;
+	            continue;                                   //{[]()}({){}{][) => {[]()}{}
 	        }
-	        else if(c=='(' && (ch==')')) {
-	            f=f+c;
-	            l=ch+l;
-	        }
-	        else if(c=='{' &&  (ch=='}')) {
-	            f=f+c;
-	            l=ch+l;
+	        else if(ch=='}' || ch==']' || ch==')') {
+	            st.pop();
+	            ss+=ch;
+	            if(st.isEmpty() || i==len-1) {
+	                //System.out.println("SubPar => "+ss);
+	                s3+=subParenthesis(ss);
+	                ss="";
+	            }
 	        }
 	    }
-	    
-	    return f+l;
+	    return s3;
+	}
+	static String subParenthesis(String ss) {
+	     int len=ss.length();
+	     String s1="", s2="", s3="";
+	     Stack<Character> st=new Stack<>();
+	     int c=0;
+	     for(int i=0;i<len;i++) {
+	        char ch=ss.charAt(i);
+	        if(ch=='{' || ch=='[' || ch=='(') {
+	            c=0;                                     
+	            st.push(ch);                              
+	        }
+	        else if(ch=='}' || ch==']' || ch==')') {     
+	            char pop=st.pop();
+	            //System.out.println(pop+" "+ch);
+	            c++;
+	            if(pop=='{' && ch=='}') {
+	                s1=pop+s1;
+	                s2=s2+ch;
+	                if(c>1) {
+	                    s3=s1+s3+s2;
+	                }
+	                else {
+	                    s3+=s1+s2;
+	                }
+	                s1=s2="";
+	            }
+	            else if(pop=='[' && ch==']') {
+	                s1=pop+s1;
+	                s2=s2+ch;
+	                if(c>1) {
+	                    s3=s1+s3+s2;
+	                }
+	                else {
+	                    s3+=s1+s2;
+	                }
+	                s1=s2="";
+	            }
+	            else if(pop=='(' && ch==')') {
+	                s1=pop+s1;
+	                s2=s2+ch;
+	                if(c>1) {
+	                    s3=s1+s3+s2;
+	                }
+	                else {
+	                    s3+=s1+s2;
+	                }
+	                s1=s2="";
+	            }
+	        } 
+	     }
+	     return s3;
 	}
 	
-	
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-        System.out.print("Give Input : ");
-		String s=sc.next();
-		for(int i=0;i<s.length();i++)
-		{
-		    char c=s.charAt(i);
-		    if(li.isEmpty() && (c=='}' || c==']' || c==')')) {
-		        continue;
-		    }
-		    else if(c=='[' || c=='{' || c=='(') {
-		        li.add(c);
-		        sn=sn+c;
-		        cou++;
-		    }
-		    else if(c==']' || c=='}' || c==')') {
-		        sn=sn+c;
-		        cou--;
-		        if(cou==0) {
-		            reducedParenthesis(sn);
-		        }
-		    }
-	    }
-	    for(String list:lis) {
-	        System.out.print(list+" ");
-	    }
-	}
 }
